50b8818f0f0409caf25efda88a0668f07fcd52d6
BrianLupu93 <brianlupu18@gmail.com>
2022-10-31 12:35:56 +0200

    2nd cleanup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Timezone from "./components/Timezone";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Timezone />
+    </div>
+  );
 }
 
 export default App;
