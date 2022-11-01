9ffe1a5bfb80fee83367fcdd39d932d2be06a07d
BrianLupu93 <brianlupu18@gmail.com>
2022-11-01 14:42:23 +0200

    time date and city fixed

--- frontend/src/components/Counter.jsx
+++ frontend/src/components/Counter.jsx
@@ -1,19 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
+import customParseFormat from "dayjs/plugin/customParseFormat";
 dayjs.extend(utc);
 dayjs.extend(timezone);
+dayjs.extend(customParseFormat);
 
 const Counter = () => {
-  const now = dayjs();
-  const local = now();
+  const [today, setToday] = useState();
+  const [localCity, setLocalCity] = useState();
 
-  console.log(timezone);
+  const timezones = ["Europe/Rome", "Asia/Tokyo", "America/Toronto"];
+
+  useEffect(() => {
+    const localCity = dayjs.tz.guess();
+    const localTime = dayjs().format("DD.MM.YYYY____HH:mm:ss");
+    setLocalCity(localCity);
+
+    setToday(localTime);
+  }, []);
+
+  useEffect(() => {
+    const newToday = dayjs().tz(localCity).format("DD.MM.YYYY____HH:mm:ss");
+    setToday(newToday);
+  }, [localCity]);
 
   return (
     <>
-      <div>Counter</div>
+      <div>
+        <div>
+          <div>
+            <h2>{localCity}</h2>
+            <h2>{today}</h2>
+          </div>
+          <select
+            name="cityName"
+            onChange={(e) => setLocalCity(e.target.value)}
+          >
+            <option key={0} value={dayjs.tz.guess()}>
+              {dayjs.tz.guess()}
+            </option>
+            {timezones.map((timezone, i) => {
+              return (
+                <option value={timezone} key={i + 1}>
+                  {timezone}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+      </div>
     </>
   );
 };
