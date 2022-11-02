f4b43604cf14fba399ebf8d1c508cb66393d6bdd
BrianLupu93 <brianlupu18@gmail.com>
2022-11-02 15:32:06 +0200

    counter interface and functionality improved

--- frontend/src/components/CounterSetUp.jsx
+++ frontend/src/components/CounterSetUp.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
@@ -13,10 +14,12 @@ const CounterSetUp = () => {
   const [today, setToday] = useState();
   const [localCity, setLocalCity] = useState();
   const [end, setEnd] = useState({});
-  const [duration, setDuration] = useState();
   const [counters, setCounters] = useState([]);
+  const [displayCounters, setDisplayCounters] = useState([]);
 
   const timezones = ["Europe/Rome", "Asia/Tokyo", "America/Toronto"];
+
+  const { register, reset } = useForm();
 
   useEffect(() => {
     const localCity = dayjs.tz.guess();
@@ -29,19 +32,23 @@ const CounterSetUp = () => {
   useEffect(() => {
     setTimeout(() => {
       setToday(dayjs().tz(localCity).format("DD-MM-YYYY,HH:mm:ss"));
+
+      if (counters.length > 0) {
+        const holdDurations = [];
+        counters.map((counter) => {
+          const duration = dayjs.duration(counter.diff(startStart));
+
+          holdDurations.push(duration.$d);
+          return holdDurations;
+        });
+        return setDisplayCounters(holdDurations);
+      }
     }, 1000);
   }, [today]);
 
-  useEffect(() => {
-    setTimeout(() => {
-      counters.map((counter) => {
-        return setDuration(counter.end.diff(counter.start));
-      });
-    }, 1000);
-  }, [counters]);
-
+  console.log(displayCounters);
   const endEnd = dayjs()
-    .set("date", end.endDate?.substring(8, 9))
+    .set("date", end.endDate?.substring(8, 10))
     .set("month", end.endDate?.substring(5, 7) - 1)
     .set("year", end.endDate?.substring(0, 4))
     .set("hour", end.endTime?.substring(0, 2))
@@ -54,21 +61,19 @@ const CounterSetUp = () => {
     .set("year", today?.substring(6, 10))
     .set("hour", today?.substring(11, 13))
     .set("minute", today?.substring(14, 16))
-    .set("second", today?.substring(17, 18));
+    .set("second", today?.substring(17, 19));
 
   const startCount = (e) => {
     e.preventDefault();
-    if (end.endDate && end.endTime) {
-      setCounters([...counters, { end: endEnd, start: startStart }]);
-    }
+    setCounters([...counters, endEnd]);
+    setEnd({});
+    reset();
   };
-
-  console.log(counters);
 
   return (
     <>
       <div>
-        <div className="setup-form">
+        <form className="setup-form">
           <div>
             <div className="display-time">
               <code>{today?.split(",")[0]}</code>
@@ -98,12 +103,14 @@ const CounterSetUp = () => {
           <div className="user-inputs">
             <label htmlFor="endDate">Event Date: </label>
             <input
+              {...register("endDate")}
               id="endDate"
               type="date"
               onChange={(e) => setEnd({ ...end, endDate: e.target.value })}
             ></input>
             <label htmlFor="endTime">Event Time: </label>
             <input
+              {...register("endTime")}
               id="endTime"
               type="time"
               onChange={(e) => setEnd({ ...end, endTime: e.target.value })}
@@ -117,15 +124,55 @@ const CounterSetUp = () => {
               <code>Start</code>
             </button>
           </div>
-        </div>
+        </form>
         <div className="display-counter">
           <div className="counter">
-            <p>Y: {duration?.$d.years}</p>
-            <p>M: {duration?.$d.months}</p>
-            <p>D: {duration?.$d.days}</p>
-            <p>H: {duration?.$d.hours}</p>
-            <p>M: {duration?.$d.minutes}</p>
-            <p>S: {duration?.$d.seconds}</p>
+            {displayCounters?.map((counter, i) => {
+              return (
+                <div key={i} className="counter-row">
+                  <div className="counter-interface">
+                    <code className="space one">{counter.years}</code>
+                    <code className="space two">{counter.months}</code>
+                    <code className="space three">{counter.days}</code>
+                    <code className="space four">{counter.hours}</code>
+                    <code className="space five">{counter.minutes}</code>
+                    <code className="space six">{counter.seconds}</code>
+                  </div>
+                  <div className="counter-legenda">
+                    <code className="space-legenda one">
+                      {counter.years === 1 || counter.years === 0
+                        ? "YEAR"
+                        : "YEARS"}
+                    </code>
+                    <code className="space-legenda two">
+                      {counter.months === 1 || counter.months === 0
+                        ? "MONTH"
+                        : "MONTHS"}
+                    </code>
+                    <code className="space-legenda three">
+                      {counter.days === 1 || counter.days === 0
+                        ? "DAY"
+                        : "DAYS"}
+                    </code>
+                    <code className="space-legenda four">
+                      {counter.hours === 1 || counter.hours === 0
+                        ? "HOUR"
+                        : "HOURS"}
+                    </code>
+                    <code className="space-legenda five">
+                      {counter.minutes === 1 || counter.minutes === 0
+                        ? "MINUTE"
+                        : "MINUTES"}
+                    </code>
+                    <code className="space-legenda six">
+                      {counter.seconds === 1 || counter.seconds === 0
+                        ? "SECOND"
+                        : "SECONDS"}
+                    </code>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
